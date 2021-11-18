98e5c3b8afce8f98f88aad82f90ae7dec439bf98
Cameron <axxes9mc@gmail.com>
2021-11-17 22:46:03 -0700

    Added random starting velocity

--- batter/game/ball.py
+++ batter/game/ball.py
@@ -1,10 +1,11 @@
 from game.actor import Actor
 from game.point import Point
+import random
 
 class Ball(Actor):
 
     # initialization *
     def __init__(self):
         super().__init__()
-        self._velocity = Point(5, 5)
+        self._velocity = Point(int(random.randint(0, 10)), 3)
         self._type = "ball"
